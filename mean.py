ddf36932648e6ba9fa83b0d149eb3cc2ee228032
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2018-07-05 22:12:17 +0200

    Clean up mean module

--- mean.py
+++ mean.py
@@ -2,13 +2,6 @@ import os
 assert True == True
 
 def mean(num_list):
-    assert isinstance(num_list, list)
-    return sum(num_list)/len(num_list)
-
-from math import isclose
-assert isclose(1.9999, 2, abs_tol=0.003)
-
-def mean(num_list):
     try:
         return sum(num_list)/len(num_list)
     except ZeroDivisionError as detail:
